79cee0c9587c031e43b9ae9582fbcb00d8210b22
abdumajid2 <abdumajidt89@gmail.com>
2026-02-27 13:00:43 +0500

    dd

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -475,7 +475,7 @@ export default function ProductsPage() {
         {filtered.map((p) => (
           <div
             key={p.id}
-            className="p-4 rounded-2xl w-80 md:w-400  mx-auto bg-white/10 border border-white/20 flex flex-col justify-center md:flex-row md:items-center md:justify-between gap-3"
+            className="p-4 rounded-2xl w-80 md:w-300  mx-auto bg-white/10 border border-white/20 flex flex-col justify-center md:flex-row md:items-center md:justify-between gap-3"
           >
             <div className="min-w-0">
               <div className="font-semibold truncate text-base md:text-lg">
